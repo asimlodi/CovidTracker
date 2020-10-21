82f719c81377f2ea37b803ff96a2f4b8d457b3e3
asimlodi <asimumair@outlook.com>
2020-10-21 11:04:21 -0700

    Updates

---
+++ precache-manifest.3d8752bd46a86c4ea2ff09699faa5a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1c9ec14e5a104ce4afb1a9431163eb",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "1100666e45b32296649d",
+    "url": "/CovidTracker/static/css/main.c94b9b32.chunk.css"
+  },
+  {
+    "revision": "b43242e562d88e97b864",
+    "url": "/CovidTracker/static/js/2.0e609135.chunk.js"
+  },
+  {
+    "revision": "a07000321a1ebac9c07126414b759ee0",
+    "url": "/CovidTracker/static/js/2.0e609135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1100666e45b32296649d",
+    "url": "/CovidTracker/static/js/main.37d9800d.chunk.js"
+  },
+  {
+    "revision": "dd0e644dc5b5b996215d",
+    "url": "/CovidTracker/static/js/runtime-main.dfc585e9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CovidTracker/static/media/image.d7265326.png"
+  }
+]);
